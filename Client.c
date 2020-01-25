964c7cbc245f0991d181368dab56972a84e3cdfe
cappadavide <davidex.somma@gmail.com>
2020-01-25 11:26:30 +0100

    Prova client server

---
+++ Client.c
@@ -0,0 +1,107 @@
+#include <netdb.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/time.h> 
+#include <sys/epoll.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <errno.h>
+#include <fcntl.h>
+#define MAX 1000 
+extern int errno;
+int epfd;
+struct epoll_event event;
+struct epoll_event events[64];
+
+struct config{
+    unsigned int port;
+    char ip[28];
+    char hostname[100];
+};
+
+void chooseServer(struct sockaddr_in *serverConfig);
+void game(int server_sd);
+int main() 
+{ 
+
+    int sockfd, connfd; 
+    pthread_t tid;
+    struct sockaddr_in serverConfig;
+    // socket create and varification 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+   
+    if (sockfd == -1){ 
+        printf("C'è stato un problema: chiusura imminente.\n"); 
+        exit(0); 
+    } 
+    else{
+        chooseServer(&serverConfig);
+        // connect the client socket to server socket 
+        if (connect(sockfd, (struct sockaddr*)&serverConfig, sizeof(serverConfig)) != 0) { 
+            //printf("Connessione con il server fallita...\n"); 
+            printf("Errore: %s\nHai inserito bene i parametri?\n",strerror(errno));
+            exit(0); 
+        } 
+        else
+            printf("Connesso al server!\n");
+            //fcntl(sockfd, F_SETFL, O_NONBLOCK); /* Change the socket into non-blocking state*/	
+            game(sockfd);
+        }
+} 
+
+void chooseServer(struct sockaddr_in *serverConfig){
+    int chooseOption;
+    unsigned int port;
+    char ip[28];
+    serverConfig->sin_family = AF_INET; 
+    printf("Benvenuto!\n");
+    printf("Specificare come vuoi selezionare il server:\n(1)Tramite IP\n(2)Tramite Hostname\n");
+    scanf("%d",&chooseOption);
+    switch (chooseOption)
+    {
+    case 1:
+        printf("Inserisci IP:\n");
+        scanf("%s",ip);
+        if(inet_aton(ip, &serverConfig->sin_addr)==0){
+            printf("Ip non valido\n");
+            exit(1);
+        }   
+        break;
+    case 2:
+        printf("Inserisci hostname\n");
+        break;
+    
+    default:
+        printf("Operazione annullata. Chiusura imminente.\n");
+        exit(0);
+        break;
+    }
+    printf("Inserisci porta\n");
+    scanf("%u",&port);
+    serverConfig->sin_port=htons(port);
+}
+
+void game(int server_sd){
+    char buffer[1000];
+    int n,num_ready,i;
+    n=read(server_sd, buffer, 1000); 
+    write(STDOUT_FILENO,buffer,n);
+    memset(buffer,'\0',sizeof(buffer));
+    n=0;
+    while(1){
+        printf("Scegli:\n");
+        scanf("%s",buffer);
+        if(strlen(buffer)>0){
+            write(server_sd,buffer,strlen(buffer));
+            memset(buffer,'\0',sizeof(buffer));
+            system("clear");
+            while(n=read(server_sd,buffer,1)>0)
+                write(STDOUT_FILENO,buffer,1);
+        }
+        n=0;
+        memset(buffer,'\0',sizeof(buffer));
+    }
+}
---
+++ Server.c
@@ -0,0 +1,146 @@
+/*Prova Server*/
+
+#include <stdio.h> 
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <unistd.h>
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <fcntl.h>
+#define MAX 1000
+#define PORT 5000
+#define SA struct sockaddr 
+
+// Login Function
+void *login(void *sockfd) 
+{ 
+    
+    char gameHome[]="----PROGETTO LSO-GIOCO----\nBenvenuto,cosa vuoi fare?\n(1)Login\n(2)Registrati\n(3)Aiuto\n(4)Informazioni sul gioco\n";
+    char buffer[MAX];
+    int n,connected=1,clientsd=*(int*)sockfd,fd;
+    write(clientsd,gameHome,sizeof(gameHome));
+
+    while(1){
+        
+        read(clientsd, buffer, sizeof(buffer));
+        if(strlen(buffer)>0){
+            printf("Listening..\n");
+            printf("Lettera: %c\n",buffer[0]);
+            if(buffer[0]=='1'){
+            }
+            else if(buffer[0]=='2'){
+
+            }
+            else if (buffer[0]=='3'){
+                memset(buffer,'\0',MAX);
+                fd=open("GameGuide.txt",O_RDONLY);
+                if(fd<0){
+                    perror("Qualcosa è andato storto");
+                }
+                else{
+                    //n=read(fd,buffer,MAX);
+                    //write(clientsd,buffer,n);
+                    while(n=read(fd,buffer,1)>0)
+                        write(clientsd,buffer,1);
+                    write(clientsd,gameHome,sizeof(gameHome));
+                    close(fd);
+                }
+            }
+            else if(buffer[0]=='4'){
+                memset(buffer,'\0',MAX);
+                fd=open("GameInfo.txt",O_RDONLY);
+                if(fd<0){
+                    perror("Qualcosa è andato storto");
+                }
+                else{
+                    //n=read(fd,buffer,MAX);
+                    //write(clientsd,buffer,n);
+                    while(n=read(fd,buffer,1)>0)
+                        write(clientsd,buffer,1);
+                    write(clientsd,gameHome,sizeof(gameHome));
+                    close(fd);
+                }
+            }
+            else if (strncmp("exit", buffer, 4) == 0) { 
+                    printf("Server Exit...\n"); 
+                    close(clientsd);
+                    pthread_exit(NULL);
+            } 
+            else{
+                write(clientsd,"Per favore, immettere una scelta valida, altrimenti exit per uscire",50);
+            }
+        }   
+        n=0;
+        memset(buffer,'\0',MAX);
+    }
+
+      
+    close(clientsd);
+    pthread_exit(NULL); 
+
+}
+  
+// Driver function 
+int main() 
+{ 
+   
+    int sockfd, connfd, len,i=0; 
+    struct sockaddr_in servaddr, cli; 
+    pthread_t tid;
+    // socket create and verification 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1) { 
+        printf("socket creation failed...\n"); 
+        exit(0); 
+    } 
+    else
+    printf("Socket successfully created..\n"); 
+    memset(&servaddr, '\0', sizeof(servaddr));
+  
+    // assign IP, PORT 
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    servaddr.sin_port = htons(PORT); 
+
+
+    // Binding newly created socket to given IP and verification 
+    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
+        printf("socket bind failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Socket successfully binded..\n"); 
+  
+    // Now server is ready to listen and verification 
+    if ((listen(sockfd, 10)) != 0) { 
+        printf("Listen failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Server listening..\n"); 
+    len = sizeof(cli); 
+    while(1){
+
+        // Accept the data packet from client and verification 
+        connfd = accept(sockfd, (SA*)&cli, &len); 
+        if(connfd>0){
+            i++;
+            int *thread_sd = (int*) malloc(sizeof(int));
+            *thread_sd =  connfd;
+            printf("server: new connection from %d %s\n",connfd,inet_ntoa(cli.sin_addr));
+            pthread_create(&tid, NULL, login, (void *) thread_sd);
+    
+        }
+        
+
+    }
+    // After chatting close the socket 
+    close(sockfd); 
+} 
+
+
