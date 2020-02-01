5eb85d36e4f083ce858fd90225e25e649ad619cc
cappadavide <davidex.somma@gmail.com>
2020-02-01 23:12:53 +0100

    Restyling del codice

---
+++ Prototipi/Client2.c
@@ -0,0 +1,144 @@
+#include <netdb.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/time.h> 
+#include <sys/epoll.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <errno.h>
+#include <fcntl.h>
+#define MAX 1000 
+#define clear() printf("\033[H\033[J")
+extern int errno;
+int epfd;
+struct epoll_event event;
+struct epoll_event events[64];
+
+struct config{
+    unsigned int port;
+    char ip[28];
+    char hostname[100];
+};
+
+void chooseServer(struct sockaddr_in *serverConfig);
+void game(int server_sd);
+void receiveMessage(int server_sd);
+int isExit(char buffer[]);
+
+int main() 
+{ 
+
+    int sockfd, connfd; 
+    pthread_t tid;
+    struct sockaddr_in serverConfig;
+    // socket create and varification 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+   
+    if (sockfd == -1){ 
+        printf("C'è stato un problema: chiusura imminente.\n"); 
+        exit(0); 
+    } 
+    else{
+        chooseServer(&serverConfig);
+        // connect the client socket to server socket 
+        if (connect(sockfd, (struct sockaddr*)&serverConfig, sizeof(serverConfig)) != 0) { 
+            //printf("Connessione con il server fallita...\n"); 
+            printf("Errore: %s\nHai inserito bene i parametri?\n",strerror(errno));
+            exit(0); 
+        } 
+        else
+            printf("Connesso al server!\n");
+            game(sockfd);
+        }
+} 
+
+void chooseServer(struct sockaddr_in *serverConfig){
+    int chooseOption;
+    unsigned int port;
+    char ip[28];
+    char hostname[100];
+    serverConfig->sin_family = AF_INET; 
+    struct hostent *p;
+    printf("Benvenuto!\n");
+    printf("Specificare come vuoi selezionare il server:\n(1)Tramite IP\n(2)Tramite Hostname\n");
+    scanf("%d",&chooseOption);
+    switch (chooseOption)
+    {
+    case 1:
+        printf("Inserisci IP:\n");
+        scanf("%s",ip);
+        if(inet_aton(ip, &serverConfig->sin_addr)==0){
+            printf("Ip non valido\n");
+            exit(1);
+        }   
+        break;
+    case 2:
+        //Da testare
+        printf("Inserisci hostname\n");
+        scanf("%s",hostname);
+        p=gethostbyname(hostname);
+        if(!p){
+            herror("gethostbyname");
+            exit(1);
+        }
+        serverConfig->sin_addr.s_addr=*(uint32_t*)(p->h_addr_list[0]);
+        break;
+    
+    default:
+        printf("Operazione annullata. Chiusura imminente.\n");
+        exit(0);
+        break;
+    }
+    printf("Inserisci porta\n");
+    scanf("%u",&port);
+    serverConfig->sin_port=htons(port);
+}
+
+void receiveMessage(int server_sd){
+    int i=0,nread,nbytes;
+    char buffer[5000];
+    read(server_sd,&nbytes,sizeof(int));//Read how many bytes server is going to send me
+    while(i<nbytes){//Ready to read message and write on STDOUT
+        nread=read(server_sd,buffer,100);//reading small chunks of bytes to avoid lost data 
+        i+=nread;
+        write(STDOUT_FILENO,buffer,nread);
+    }
+}
+
+int isExit(char buffer[]){
+    if(strlen(buffer)==4){
+        if(memcmp(buffer,'exit',4)==0)
+            return 1;
+    }
+    return 0;
+}
+
+
+void game(int server_sd){
+    char buffer[5000];
+    int n,num_ready,i,nread;
+    receiveMessage(server_sd); 
+    memset(buffer,'\0',sizeof(buffer));
+    while(1){
+        printf("Scegli:\n");
+        scanf("%s",buffer);
+        if(strlen(buffer)>0){
+            if(isExit(buffer)){
+                close(server_sd);
+                printf("Disconnesso.\n");
+                break;
+            }
+            n=strlen(buffer);
+            write(server_sd,&n,sizeof(int));//Tell to server how many bytes I'm going to send him
+            write(server_sd,buffer,strlen(buffer));//Then I send data
+            memset(buffer,'\0',sizeof(buffer));//Clear buffer
+            system("clear");//Clear shell for a better readability
+            receiveMessage(server_sd);
+  
+        }
+        memset(buffer,'\0',sizeof(buffer));
+    }
+}
---
+++ Prototipi/Server2.c
@@ -0,0 +1,166 @@
+/*Prova Server*/
+
+#include <stdio.h> 
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <unistd.h>
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <fcntl.h>
+#define MAX 5000
+#define PORT 5000
+#define SA struct sockaddr 
+
+// Game Function
+void *gameThread(void *sockfd) 
+{ 
+    int clientsd=*(int*)sockfd;
+    gameMenu(clientsd);
+
+    /*close(clientsd);
+    pthread_exit(NULL);*/
+
+}
+
+void gameMenu(int clientsd);
+int isClientConnClose(int clientsd,char buffer[]);
+
+// Driver function 
+int main() 
+{ 
+   
+    int sockfd, connfd, len,i=0; 
+    struct sockaddr_in servaddr, cli; 
+    pthread_t tid;
+    // socket create and verification 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1) { 
+        printf("socket creation failed...\n"); 
+        exit(0); 
+    } 
+    else
+    printf("Socket successfully created..\n"); 
+    memset(&servaddr, '\0', sizeof(servaddr));
+  
+    // assign IP, PORT 
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    servaddr.sin_port = htons(PORT); 
+
+
+    // Binding newly created socket to given IP and verification 
+    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
+        printf("socket bind failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Socket successfully binded..\n"); 
+  
+    // Now server is ready to listen and verification 
+    if ((listen(sockfd, 10)) != 0) { 
+        printf("Listen failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Server listening..\n"); 
+    len = sizeof(cli); 
+    while(1){
+
+        // Accept the data packet from client and verification 
+        connfd = accept(sockfd, (SA*)&cli, &len); 
+        if(connfd>0){
+            i++;
+            int *thread_sd = (int*) malloc(sizeof(int));
+            *thread_sd =  connfd;
+            printf("server: new connection from %d %s\n",connfd,inet_ntoa(cli.sin_addr));
+            pthread_create(&tid, NULL, gameThread, (void *) thread_sd);
+    
+        }
+        
+
+    }
+    close(sockfd); 
+} 
+void gameMenu(int clientsd){
+    char gameHome[]="\n----PROGETTO LSO-GIOCO----\nBenvenuto,cosa vuoi fare?\n(1)Login\n(2)Registrati\n(3)Aiuto\n(4)Informazioni sul gioco\n";
+    char buffer[MAX];
+    int n,nb,fd,nread;
+    nb=sizeof(gameHome);
+    write(clientsd,&nb,sizeof(int));//Tell to client how many bytes is going to receive
+    write(clientsd,gameHome,sizeof(gameHome));//Send menu interface
+
+    while(1){
+        if(read(clientsd,&nb,sizeof(int))!=-1){//Read how many bytes client is going to send me
+            read(clientsd, buffer, nb); //Read data
+            if(strlen(buffer)>0){
+                if(isClientConnClose(clientsd,buffer)){
+                    printf("Closing connection...\n");
+                    close(clientsd);
+                    pthread_exit(NULL); 
+                }
+                if(buffer[0]=='1'){
+                }
+                else if(buffer[0]=='2'){
+
+                }
+                else if (buffer[0]=='3'){
+                    memset(buffer,'\0',MAX);
+                    fd=open("GameGuide.txt",O_RDONLY);
+                    if(fd<0){
+                        perror("Qualcosa è andato storto");
+                    }
+                    else{
+                        n=0;
+                        while((nread=read(fd,buffer,5))>0){//Reading file in little chunks of bytes
+                            n+=nread;
+                        }
+                        printf("n %d\n",n);
+                        strcat(buffer,gameHome);
+                        nb=n+sizeof(gameHome);//Send Guide + Menu
+                        write(clientsd,&nb,sizeof(int));//Tell to client I'm going to send him nb bytes
+                        write(clientsd,buffer,nb);//Send data
+                        close(fd);
+                    }
+                }
+                else if(buffer[0]=='4'){
+                    memset(buffer,'\0',MAX);
+                    fd=open("GameInfo.txt",O_RDONLY);
+                    if(fd<0){
+                        perror("Qualcosa è andato storto");
+                    }
+                    else{
+                        n=0;
+                        while((nread=read(fd,buffer,5))>0){//Reading file in little chunks of bytes
+                            n+=nread;
+                        }
+                        printf("n %d\n",n);
+                        strcat(buffer,gameHome);
+                        nb=n+sizeof(gameHome);//Send Info + Menu
+                        write(clientsd,&nb,sizeof(int));//Tell to client I'm going to send him nb bytes
+                        write(clientsd,buffer,nb);//Send data
+                        close(fd);
+                    }
+                }
+                else{
+                    write(clientsd,"Per favore, immettere una scelta valida, altrimenti exit per uscire",50);
+                }
+            }   
+        }
+        n=0;
+        memset(buffer,'\0',MAX);
+    }
+
+}
+int isClientConnClose(int clientsd,char buffer[]){
+    if(strlen(buffer)==4){
+        if(memcmp(buffer,'exit',4))
+            return 1;
+    }
+    return 0;
+
+}
