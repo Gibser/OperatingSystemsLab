1c975e155378ca72d7c053c0afd02b533efd4082
cappadavide <davidex.somma@gmail.com>
2020-02-16 18:48:06 +0100

    aggiornato

--- Prototipi/matrixpunz.c
+++ Prototipi/matrixpunz.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h> 
+#define MAX_USERS 8
 
 struct cell{
   int playerSD; //Socket descriptor
@@ -12,12 +13,24 @@ struct cell{
   char object[10];
   
 };
-void initializeMatrix(int rows,int cols);
-void createMap(int rows,int cols);
-void printMatrix(int rows,int cols);
+void initializeMatrix();
+void createMap();
+void printMatrix();
+void spawnPlayer();
+int isCellGood(struct cell a,int index1,int index2);
+int isCellFree(struct cell a);
+int isLeftFree(int index1,int index2);
+int isRightFree(int index1,int index2);
+int isUpFree(int index1,int index2);
+int isDownFree(int index1,int index2);
+char getLetter();
+
 time_t t;
 
 char items[3][10]={"sword","gold","food"};
+int mapPlayers[MAX_USERS]={0};
+int rows,cols;
+
 struct cell **map;
 
 /*  int **arr = (int **)malloc(r * sizeof(int *)); 
@@ -39,7 +52,7 @@ int main(){
    return 0;
 }
 
-void initializeMatrix(int rows,int cols){
+void initializeMatrix(){
   int i=0,j=0;
   map=(struct cell**)malloc(rows * sizeof(struct cell *));
   for(i=0;i<rows;i++){
@@ -56,11 +69,10 @@ void initializeMatrix(int rows,int cols){
 
 }
 
-void createMap(int rows,int cols){
+void createMap(){
   int wareHouses, obstacles=-1, n_items;
   int i;
   int r,c,index;
-  srand((unsigned) time(&t));
   printf("Choosing number of warehouses...\n");
   wareHouses=rand()%4+1; 
   printf("Number of Warehouses: %d\n",wareHouses);
@@ -106,7 +118,7 @@ void createMap(int rows,int cols){
 
 }
 
-void printMatrix(int rows,int cols){
+void printMatrix(){
   int i,j;
   printf("  ");
   for(i=0;i<cols;i++){
@@ -117,20 +129,20 @@ void printMatrix(int rows,int cols){
     printf("| ");
     for(j=0;j<cols;j++){
       if(map[i][j].isWareHouse==1){
-        printf("M ");
+        printf("ħ ");
       }
       else if(map[i][j].isObstacle==1){//Dovrà essere nascosto in un primo momento al client
         printf("x ");
       }
       else if(strlen(map[i][j].object)>0){
         if(strcmp(map[i][j].object,"gold")==0){
-          printf("G ");
+          printf("$ ");
         }
         else if(strcmp(map[i][j].object,"sword")==0){
-          printf("S ");
+          printf("ł ");
         }
         else if(strcmp(map[i][j].object,"food")==0){
-          printf("F ");
+          printf("@ ");
         }
       }
       else{
@@ -146,3 +158,71 @@ void printMatrix(int rows,int cols){
   }
   printf("\n");
 }
+char getLetter(){
+  int i;
+  char c;
+  for(i=0;i<MAX_USERS;i++){
+    if(mapPlayers[i]==0){
+      mapPlayers[i]=1;//Inserire al posto di 1 mettiamo socketdescriptor
+      c=(char)(i+65);
+      break;
+    }
+  }
+  return c;
+}
+int isLeftFree(int index1,int index2){
+  if(index2-1>=0){
+    return isCellFree(map[index1][index2-1]);
+  }
+  return 0;
+}
+int isRightFree(int index1,int index2){
+  if(index2+1<=cols-1){
+    return isCellFree(map[index1][index2-1]);
+  }
+  return 0;
+}
+int isUpFree(int index1,int index2){
+  if(index1-1>=0){
+    return isCellFree(map[index1-1][index2]);
+  }
+  return 0;
+}
+int isDownFree(int index1,int index2){
+  if(index1+1<=rows-1){
+    return isCellFree(map[index1+1][index2]);
+  }
+  return 0;
+}
+int isCellFree(struct cell a){
+  if(a.isObstacle==0&&a.isWareHouse==0&&a.playerSD==-1&&strlen(a.object)==0)
+    return 1;
+  return 0;
+}
+
+int isCellGood(struct cell a,int index1,int index2){
+  int exp;
+  if(isCellFree(a)){
+    exp=isLeftFree(index1,index2)+isRightFree(index1,index2)+isUpFree(index1,index2)+isDownFree(index1,index2);
+    if(exp>0)//Se almeno una cella è libera attorno al giocatore okay
+      return 1;
+    return 0;
+  }
+}
+
+
+
+void spawnPlayer(){
+  char c;
+  int index1,index2; //Potrebbero trovarsi all'esterno, quindi magari devono essere puntatori a quegli indici
+  c=getLetter();
+  while(1){
+    index1=rand()%rows;
+    index2=rand()%cols; //Cerca indici buoni finché non otteniamo una cella libera e non scomoda
+    if(isCellGood(map[index1][index2],index1,index2)){
+      break;
+    }
+  }
+  //map[index1][index2].playerSD=SOCKETDESCRIPTOR
+}
+
