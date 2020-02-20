90823f62cc720d9994339bc56adabc69596a8652
cappadavide <davidex.somma@gmail.com>
2020-02-20 15:04:50 +0100

    Client-Server communication improvements

--- Server.c
+++ Server.c
@@ -140,12 +140,12 @@ void game(int clientsd){
               break;
           }
       }
-      if(isLogged){
+      /*if(isLogged){
         printf("classifica \n%s\n",scoreboardString);
         write(clientsd,&(int){0},sizeof(int));
         write(clientsd,&(int){0},sizeof(int));
         sendMessage(clientsd,scoreboardString);
-      }
+      }*/
       
     }
 }
